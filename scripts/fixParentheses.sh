020aa00185a3ff642f402c9b841dae5dce369707
Niko Darci-Maher <niko.darcimaher@gmail.com>
2019-08-14 14:58:28 -0700

    organized everything

---
+++ scripts/fixParentheses.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+indir=$1
+
+ls $indir | grep -E "[)(]" > ~/data_reusability/fixthese.txt
+
+while read dir
+do
+
+	newname=$(echo $dir | sed -e 's/(/_/g' | sed -e 's/)/_/g' | sed -e 's/,/_/g')
+	mv $indir/$dir $indir/$newname
+
+done < ~/data_reusability/fixthese.txt
