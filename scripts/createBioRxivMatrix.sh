81388abc9ab565f84b2988b83d49e3d91e2183de
Niko Darci-Maher <niko.darcimaher@gmail.com>
2019-11-24 11:38:59 -0800

    tinkering with author script, biorxiv data

--- scripts/createBioRxivMatrix.sh
+++ scripts/createBioRxivMatrix.sh
@@ -7,46 +7,46 @@ date=$(date +"%m-%d-%Y")
 cd $indir
 
 # Extract all the papers we care about from journalWanted
-grep -o -r -E -H -e "[SDE]R[APXRSZ][0-9]{6,7}" -e "PRJNA[0-9]{6,7}" -e "G[DSP][SEL][0-9]{1,6}" -e "TCGA" -e "GTEx" $indir > ~/data_reusability/origData.txt
+grep -o -r -E -H -e "[SDE]R[APXRSZ][0-9]{6,7}" -e "PRJNA[0-9]{6,7}" -e "G[DSP][SEL][0-9]{1,6}" -e "TCGA" -e "GTEx" $indir > ~/data_reusability/scripts/origData.txt
 
-echo doi,repo_ID > ~/data_reusability/doiAndRepoIDs.txt
+cd ~/data_reusability/scripts/
+
+echo doi,repo_ID > doiAndRepoIDs.txt
 
 # Make a column of PMC IDs
-echo "PMC_ID,repo_ID" > ~/data_reusability/pmcAndRepoIDs.txt
-awk -F "[/.:]" '{print $7"."$8","$10}' ~/data_reusability/origData.txt >> ~/data_reusability/doiAndRepoIDs.txt
+echo "PMC_ID,repo_ID" > pmcAndRepoIDs.txt
+awk -F "[/.:]" '{print $7"."$8","$10}' origData.txt >> doiAndRepoIDs.txt
 
 # Make a column of repository names
-echo "repo_Name" > ~/data_reusability/repoNames.txt
-echo "journal_Name" > ~/data_reusability/journalNames.txt
+echo "repo_Name" > repoNames.txt
+echo "journal_Name" > journalNames.txt
 
 while read line; do
-	echo BioRxiv >> ~/data_reusability/journalNames.txt
+	echo BioRxiv >> journalNames.txt
 	if [[ ! -z "$(echo $line | grep -o -E -e 'SR[APXRSZ]' -e 'PRJNA')" ]]
 	then
-		echo "NCBI_SRA" >> ~/data_reusability/repoNames.txt
+		echo "NCBI_SRA" >> repoNames.txt
 	elif [[ ! -z "$(echo $line | grep -o -E 'ER[APXRSZ]')" ]]
 	then
-		echo "EMBL" >> ~/data_reusability/repoNames.txt
+		echo "EMBL" >> repoNames.txt
 	elif [[ ! -z "$(echo $line | grep -o -E 'DR[APXRSZ]')" ]]
 	then	
-		echo "DDBJ" >> ~/data_reusability/repoNames.txt	
+		echo "DDBJ" >> repoNames.txt	
 	elif [[ ! -z "$(echo $line | grep -o -E 'G[DSP][SEL][0-9]{1,6}')" ]]
 	then
-		echo "NCBI_GEO" >> ~/data_reusability/repoNames.txt
+		echo "NCBI_GEO" >> repoNames.txt
 	elif [[ ! -z "$(echo $line | grep -o 'TCGA')" ]]
 	then
-		echo "TCGA" >> ~/data_reusability/repoNames.txt
+		echo "TCGA" >> repoNames.txt
 	elif [[ ! -z "$(echo $line | grep -o 'GTEx')" ]]
 	then
-		echo "GTEx" >> ~/data_reusability/repoNames.txt 
+		echo "GTEx" >> repoNames.txt 
 	else
-		echo "other" >> ~/data_reusability/repoNames.txt
+		echo "other" >> repoNames.txt
 	fi
-done < ~/data_reusability/origData.txt
+done < origData.txt
 
 # Make a column of journal names
-
-cd ~/data_reusability/
 
 paste -d "," journalNames.txt doiAndRepoIDs.txt repoNames.txt > $outfile
 
--- scripts/extractAuthors.sh
+++ scripts/extractAuthors.sh
@@ -9,5 +9,8 @@ module load python/3.7.2
 
 echo 'authors' > $outfile
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # get script directory
-python3 $DIR/extractAuthors.py $infile >> $outfile
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # get script directory
+# python3 $DIR/extractAuthors.py $infile >> $outfile
+
+python3 ./extractAuthors.py $infile >> $outfile
+
