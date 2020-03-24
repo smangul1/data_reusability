7e7c8169e0b571c341e05a5d3189d909b3981ed8
Niko Darci-Maher <niko.darcimaher@gmail.com>
2020-03-24 13:55:09 -0700

    higher date resolution

--- scripts/extractDate.py
+++ scripts/extractDate.py
@@ -3,21 +3,39 @@ import sys
 
 filename=sys.argv[1]
 
-def extractMinDate(paperName):
-	tree=ET.parse(paperName)
-	root=tree.getroot()
-	list=[]
-	for i in root.findall("./front/article-meta/pub-date/year"):
-		list.append(i.text)
-	if len(list)==0:
-		year='NA'
+def extractFirstDate(paperName):
+	try:
+		root=ET.parse(paperName).getroot()
+	except:
+		return "missing_file:" + paperName	
+	years=[]
+	mons=[]
+	days=[]
+	for y in root.findall("./front/article-meta/pub-date/year"):
+		years.append(y.text)
+	for m in root.findall("./front/article-meta/pub-date/month"):
+		mons.append(m.text)
+	for d in root.findall("./front/article-meta/pub-date/day"):
+		days.append(d.text)
+	if len(years) == 0:
+		return 'NaN'
 	else:
-		year=min(list)
-	return year
+		yr = years[0]
+	if len(mons) == 0:
+		mn = '01'
+	else:
+		mn = mons[0]
+	if len(days) == 0:
+		dy = '01'
+	else:
+		dy = days[0]
+	date = yr + '-' + mn + '-' + dy
+	return date
 
 file=open(filename)
 list=[line[:-1] for line in file]
 
 for i in list:
-	print(extractMinDate(i))
+	p = i.split('/')[-1].split('.')[0]
+	print(p + ',' + extractFirstDate(i))
 
--- scripts/extractDate.sh
+++ scripts/extractDate.sh
@@ -6,5 +6,5 @@ outfile=$2
 #. /u/local/Modules/default/init/modules.sh
 #module load python/3.7.2
 
-echo 'date' > $outfile
+echo 'pmc_ID,date' > $outfile
 python ./extractDate.py $infile >> $outfile
