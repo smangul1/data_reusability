aca1a88045fd95e04c5cfba540ce0a5957e28f3e
Niko Darci-Maher <niko.darcimaher@gmail.com>
2020-07-09 18:11:03 -0700

    doomed mouse strain side project

--- scripts/mouseStrainScrape.sh
+++ scripts/mouseStrainScrape.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -l h_data=48G,h_rt=24:00:00
+#$ -l h_data=8G,h_rt=2:00:00
 #$ -o mouseStrainScrape.sh.log
 
-indir="/u/scratch/n/nikodm/pmcOA_05-05-2020/"
 outfile="../data_tables/mouse_strains.csv"
-pathfile="../data_tables/MINImouse_paths.csv"
+pathfile="../data_tables/mouse_paths.csv"
 
+rm -f rawStrainData.txt
 touch rawStrainData.txt
 
 for pap in $( cat $pathfile ); do
-    grep -o -i -E \
-    -e "Mus musculus\S*" \
-    -e "Mus musculus domesticus\S*" \
-    -e "Mus musculus molossinus\S*" \
-    -e "Mus musculus musculus\S*" \
-    -e "Mus musculus castaneus\S*" \
-    -e "Mus spretus\S*"
+    grep -o -i -E -H \
+    -e "Mus musculus[^[:space:]^<]*" \
+    -e "Mus musculus domesticus[^[:space:]^<]*" \
+    -e "Mus musculus molossinus[^[:space:]^<]*" \
+    -e "Mus musculus musculus[^[:space:]^<]*" \
+    -e "Mus musculus castaneus[^[:space:]^<]*" \
+    -e "Mus spretus[^[:space:]^<]*" \
     $pap >> rawStrainData.txt
-
 done
+
+echo "journal,pmc_ID" > journal_pmc.txt
+awk -F "[/.]" '{print $7","$8}' rawStrainData.txt >> journal_pmc.txt
+echo "mouse_strain" > str.txt
+awk -F ":" '{print $2}' rawStrainData.txt >> str.txt
+paste -d ',' journal_pmc.txt str.txt > $outfile
+rm -f journal_pmc.txt str.txt rawStrainData.txt
